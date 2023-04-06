c90fda62871be6ce1a99dce025620d1a6023cc71
rituneeta <rituneeta@gmail.com>
2023-04-06 18:16:26 +0530

    add swagger documentation

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { initDbConnection } from "./database.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +13,8 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/api-swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, async (error) => {
     if (!error) {
---
+++ src/swagger.js
@@ -0,0 +1,29 @@
+const swaggerDocument = {
+    "swagger": "2.0",
+    "info": {
+        "version": "1.0.0",
+        "title": "User Wallet System Apis"
+    },
+    "basePath": "/api",
+    "schemes": [
+        "http",
+        "https"
+    ],
+    "consumes": [
+        "application/json",
+        "application/x-www-form-urlencoded",
+        "multipart/form-data"
+    ],
+    "produces": [
+        "application/json"
+    ],
+    "tags": [
+        {
+            "name": "User",
+            "description": "User related api's"
+        }
+    ],
+    "paths": {}
+}
+
+export default swaggerDocument;
