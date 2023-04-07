279abfdb64911a2863bd5d9c421c6d415c7387ef
rituneeta <rituneeta@gmail.com>
2023-04-07 20:08:48 +0530

    update wallet-send service

--- src/services/user/userWallet.service.js
+++ src/services/user/userWallet.service.js
@@ -28,19 +28,15 @@ export const sendWalletService = async (userId, params) =>{
 
    const userDetail = await selectOne(userModel, { id: userId });
    if (params?.passcode == userDetail.passcode) {
-      const walletDetail = await selectOne(walletModel, { id: userId });
+      const walletDetail = await selectOne(walletModel, { userId: userId });
       let senderUserWalletData = walletDetail.get({ plain: true });
-      console.log("Sender data", senderUserWalletData, userId)
       if(!_.isEmpty(walletDetail)){
          if(params.amount > senderUserWalletData.amount){
             throw new Error(MESSAGES.insufficient_amount)
          }else{
             const { amount, receiverPhoneNumber} = params;
-   
             const receiverWalletData = await selectOne(walletModel, { phoneNumber: receiverPhoneNumber });
             let receiverDetails = receiverWalletData.get({ plain: true });
-            console.log("receiver data", receiverDetails)
-
             if(!_.isEmpty(receiverDetails)){
                const addDataObj= {
                   senderUserId: userId,
@@ -55,7 +51,6 @@ export const sendWalletService = async (userId, params) =>{
                   await updateData({ model: walletModel, amount: receiverDetails.amount + (+params.amount) }, { where: { userId: receiverDetails.userId } });
                   await updateData({ model: walletModel, amount: senderUserWalletData.amount - (+params.amount) }, { where: { userId } });
                }else{
-                  console.log(senderUserWalletData.amount + (+params.amount) , "amount", senderUserWalletData.amount,params.amount )
                   await updateData({ model: walletModel, amount: senderUserWalletData.amount + (+params.amount) }, { where: { userId } });
                }
             }else{
